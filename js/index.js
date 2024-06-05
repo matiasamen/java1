7ad49a2d7c15efd5085132e54d7ce9721ffe454a
matiasAmen1 <matiasamen202@gmail.com>
2024-06-05 20:27:05 -0300

    modificaciones nuevas javascript

--- js/index.js
+++ js/index.js
@@ -7,3 +7,26 @@ if (email && password) {
 } else {
     console.log("Por favor, complete todos los campos obligatorios.");
 }
+
+const EDAD = prompt("ingresar edad del usuario");
+const EDAD_NUMEROS = parseInt(EDAD);
+
+if(isNaN(EDAD_NUMEROS))
+    {console.log("poner solamente valores numericos");}
+
+else{
+    if(EDAD_NUMEROS >=18)
+        {console.log("buen viaje,permiso concedido");}
+
+else if(EDAD_NUMEROS == 16 || EDAD_NUMEROS == 17)
+    {console.log("podras viajar con el permiso de tus padres")
+let permisoPadre=prompt("tenes el permiso de tus padres responder si o no");
+if(permisoPadre=="si") 
+    {console.log("buen viaje,permiso concedido");}
+else(permisoPadre==="no")
+{console.log("sin permiso no podras viajar,vuelva cuando lo tenga");}
+
+}
+else if(EDAD_NUMEROS<=15)
+    {console.log("no cumples ningun requisito,viaje denegado");}
+}
